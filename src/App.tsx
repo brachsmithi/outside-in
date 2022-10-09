b39fad205b5c8b31bb188f4655257a4d3faf9392
brachsmithi <bsmithi.impaired@gmail.com>
2022-10-08 22:28:32 -0400

    complete happy path for single incomplete frame
    The focus leads entry through the frame but does not prevent edits. The
    layout is not started. Everything is directly inside the App component.
    Much refactoring needs to be done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [total, setTotal] = useState<number|null>(null)
+  const throwOneInput = useRef<HTMLInputElement>(null)
+  const throwTwoInput = useRef<HTMLInputElement>(null)
+  const closeSecondThrow = () => {
+    if (throwOneInput.current !== null && throwTwoInput.current !== null) {
+      throwTwoInput.current.blur()
+      const value1 = Number(throwOneInput.current.value)
+      const value2 = Number(throwTwoInput.current.value)
+      setTotal(value1 + value2)
+    }
+  }
+  useLayoutEffect(() => {
+    throwOneInput.current !== null && throwOneInput.current.focus()
+  })
   return (
-    <input type='text' data-cy='throw1'/>
+    <>
+      <input type='text' data-cy='throw1' ref={throwOneInput} onChange={() => throwTwoInput.current !== null && throwTwoInput.current.focus()}/>
+      <input type='text' data-cy='throw2' ref={throwTwoInput} onChange={closeSecondThrow}/>
+      <span data-cy='total'>{total}</span>
+    </>
   );
 }
 
