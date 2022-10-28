88d215420404e9f416e9e09e4305802ebe07b71b
brachsmithi <bsmithi.impaired@gmail.com>
2022-10-28 06:51:38 -0400

    reduce complication of scoring logic
    The score resolver used a lot of helper functions to grab information
    about the previous and next frames. Instead of iterating over frames,
    it now iterates over a view of frames, including the previous and next.
    This makes it substantially easier to follow the logic.
    
    Another complexity was the need to constantly cast the incoming values
    to numbers. That's unavoidable, but now that casting is centralized in
    an add function with a simple interface. The internals of that function
    are gross, but the call sites are much easier to read now.

--- src/functions/calculations.ts
+++ src/functions/calculations.ts
@@ -1,31 +1,34 @@
 import { FrameDescription } from "../models/FrameDescription";
 import { isExtraFrame } from "./analyzers";
 
+function lensOn(frameDescriptions: FrameDescription[], index: number) {
+  return [
+      frameDescriptions[index - 1] ?? null,
+      frameDescriptions[index],
+      frameDescriptions[index + 1] ?? null
+  ]
+}
+
+function add(base: number, ...addends: Array<number | string | null>) {
+  return addends.reduce((acc: number, addend) => {
+    return acc + Number(addend)
+  }, base)
+}
+
 export function resolveScores(frameDescriptions: FrameDescription[]) {
-  function cumulativeScore(index: number) {
-    if (index === 0) {
-      return 0
-    }
-    return frameDescriptions[index - 1].score
-  }
-  function nextFrameFirstThrow(index: number) {
-    return frameDescriptions[index + 1].firstThrow
-  }
-  function previousState(index: number) {
-    return frameDescriptions[index - 1].frameState
-  }
-  frameDescriptions.forEach((description) => {
-    if (description.frameState === 'Done' && description.score === null) {
-      description.score = (cumulativeScore(description.index) ?? 0) + Number(description.firstThrow) + Number(description.secondThrow)
-    } else if (description.frameState === 'Pending') {
-      const firstThrowOfNextFrame = nextFrameFirstThrow(description.index)
-      if (firstThrowOfNextFrame) {
-        description.score = (cumulativeScore(description.index) ?? 0) + 10 + Number(firstThrowOfNextFrame)
+  for (let i = 0; i < frameDescriptions.length; i++) {
+    const [previous, current, next] = lensOn(frameDescriptions, i)
+    const cumulativeScore = previous?.score ?? 0
+    if ( current.score === null && current.frameState === 'Done') {
+      current.score = add(cumulativeScore, current.firstThrow, current.secondThrow)
+    } else if (current.frameState === 'Pending') {
+      if (next?.firstThrow) {
+        current.score = add(cumulativeScore, 10, next.firstThrow)
       }
-    } else if (isExtraFrame(description) && previousState(description.index) === 'Pending') {
-      if (description.firstThrow) {
-        description.score = (cumulativeScore(description.index) ?? 0) + Number(description.firstThrow)
+    } else if (isExtraFrame(current) && previous?.frameState === 'Pending') {
+      if (current.firstThrow) {
+        current.score = add(cumulativeScore, current.firstThrow)
       }
     }
-  })
+  }
 }
