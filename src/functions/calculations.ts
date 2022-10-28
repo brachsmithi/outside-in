31faa651871f80bf275d528cc0f551cfcb2dc9c3
brachsmithi <bsmithi.impaired@gmail.com>
2022-10-28 07:18:20 -0400

    prevent rescoring in score resolver
    The only scoring condition that had a guard around it was for the frame
    state 'Done', but all of them needed it, so now the guard is around the
    block of if statements.
    
    Other sub conditions have moved into the main if statements.

--- src/functions/calculations.ts
+++ src/functions/calculations.ts
@@ -18,15 +18,13 @@ function add(base: number, ...addends: Array<number | string | null>) {
 export function resolveScores(frameDescriptions: FrameDescription[]) {
   for (let i = 0; i < frameDescriptions.length; i++) {
     const [previous, current, next] = lensOn(frameDescriptions, i)
-    const cumulativeScore = previous?.score ?? 0
-    if ( current.score === null && current.frameState === 'Done') {
-      current.score = add(cumulativeScore, current.firstThrow, current.secondThrow)
-    } else if (current.frameState === 'Pending') {
-      if (next?.firstThrow) {
+    if (current.score === null) {
+      const cumulativeScore = previous?.score ?? 0
+      if (current.frameState === 'Done') {
+        current.score = add(cumulativeScore, current.firstThrow, current.secondThrow)
+      } else if (current.frameState === 'Pending' && next?.firstThrow) {
         current.score = add(cumulativeScore, 10, next.firstThrow)
-      }
-    } else if (isExtraFrame(current) && previous?.frameState === 'Pending') {
-      if (current.firstThrow) {
+      } else if (isExtraFrame(current) && previous?.frameState === 'Pending' && current.firstThrow) {
         current.score = add(cumulativeScore, current.firstThrow)
       }
     }
